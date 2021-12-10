38a691cdd1bac3992368ecc5066806ddbe35c7b3
prasadt <prasadt@fold.health>
2021-12-10 17:04:18 +0530

    Grid added

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,33 @@
 
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import {Button} from '@ant-design/react-native';
+import {Button} from '@ant-design/react-native/lib/button';
+import Grid from '@ant-design/react-native/lib/grid';
 
 const App = () => {
+  const data: Array<{icon: string; text: string}> = [
+    {icon: 'data', text: 'Name'},
+    {icon: 'data', text: 'Status'},
+    {icon: 'data', text: 'Cohort size'},
+    {icon: 'data', text: 'Tags'},
+    {icon: 'data', text: 'Actions'},
+  ];
+
+  const renderTableItem = (el: {icon: string; text: string}, index: number) => {
+    return (
+      <Text>
+        {el.text} - {index}
+      </Text>
+    );
+  };
+
   return (
     <SafeAreaView style={{justifyContent: 'center', alignItems: 'center'}}>
       <View>
         <Text style={{fontSize: 100}}>Hello world!</Text>
       </View>
       <Button>Test</Button>
+      <Grid data={data} columnNum={5} renderItem={renderTableItem} />
     </SafeAreaView>
   );
 };
